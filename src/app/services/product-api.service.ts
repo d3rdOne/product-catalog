b05ae96bd6267632b98c9b4863fc2ae5d656cec6
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-09-01 20:50:30 +0800

    Added API service for products

---
+++ src/app/services/product-api.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../shared/models/product';
+import { environment } from 'src/environments/environment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductApiService {
+
+  private apiUrl = environment.API_BASE_URL;
+  constructor(private httpClient: HttpClient) {}
+
+  fetchProducts(): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`${this.apiUrl}/assets/product-data.json`)
+  }
+
+}
