bbf4c518aac6daf9f350d41c26f2df416d8ca472
d3rdone <19626442+d3rdOne@users.noreply.github.com>
2024-09-01 20:46:14 +0800

    Updated product card discount calculation Added product card component unit test

--- src/app/shared/components/product-card/product-card.component.spec.ts
+++ src/app/shared/components/product-card/product-card.component.spec.ts
@@ -2,19 +2,28 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { ProductCardComponent } from './product-card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { Product } from '../../models/product';
 
 describe('ProductCardComponent', () => {
   let component: ProductCardComponent;
   let fixture: ComponentFixture<ProductCardComponent>;
+  let productMock: Product = {
+    id: 1,
+    name: 'product 1',
+    description: 'product description',
+    discount: 50,
+    price: 1000
+  }
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductCardComponent ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductCardComponent);
     component = fixture.componentInstance;
+    component.product = productMock
     fixture.detectChanges();
   }));
 
